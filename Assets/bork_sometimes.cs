bad9af73c5e8383df81c59497eb2c1502cb12b67
Anthony Biondo <tonyb@tonybox.net>
2018-01-14 22:37:31 -0500

    Amazing iamdog-bork branch, now with 100% more bork!

---
+++ Assets/bork_sometimes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bork_sometimes : MonoBehaviour {
+
+    private AudioSource source;
+    public AudioClip bork;
+    
+	// Use this for initialization
+	void Start () {
+         source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Random.Range(1,100) == 50) {
+            source.PlayOneShot(bork, 0.7F);
+        }
+	}
+}
---
+++ Assets/dog_mover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dog_mover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   //var dt = Time.deltaTime;
+	   //var translation = new Vector3(0, 0 ,dt*1.5f);
+	   //this.transform.Translate(translation);
+	   
+	   var st = (float)(System.Math.Sin(Time.fixedTime));
+   	   var ct = (float)(System.Math.Cos(Time.fixedTime));
+	   
+	   this.transform.rotation = Quaternion.Euler(0, (st*180)+180, (ct*180)+180);
+	   
+       this.transform.position = new Vector3(st*10, ct*5, 0);
+	   
+	   
+	}
+}
+
