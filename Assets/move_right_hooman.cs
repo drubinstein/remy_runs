65c5441efa37e84d0611e9e9592ff6671e31288a
David Rubinstein <drubinstein91@gmail.com>
2018-01-15 08:41:35 -0500

    Press -> to move hooman right

--- Assets/move_right_hooman.cs
+++ Assets/move_right_hooman.cs
@@ -12,9 +12,15 @@ public class move_right_hooman : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if( ! collide_with_doggo )
-			transform.Translate (new Vector3 (.05f, 0, 0));
-	}
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (!collide_with_doggo)
+            {
+                transform.Translate(new Vector3(.25f, 0, 0));
+                GetComponent<Camera>().transform.Translate(new Vector3(.25f, 0, 0));
+            }
+        }
+    }
 
 	void OnCollisionEnter2D(Collision2D coll) {
 		if (coll.gameObject.tag == "doggo")
