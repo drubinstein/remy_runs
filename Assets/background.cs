7a53035aeb79ef086537aec13e3626ea953eb4f3
Benjamin <hahnehead@yahoo.com>
2018-01-15 21:37:39 -0500

    created a looping background script

---
+++ Assets/background.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background : MonoBehaviour {
+    private float pane_length;
+    private float scroll_speed = 10f;
+    // Use this for initialization
+    void Start () {
+        pane_length = transform.GetChild(0).GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            this.transform.Translate(new Vector3(scroll_speed * Time.deltaTime, 0, 0));
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            this.transform.Translate(new Vector3(-scroll_speed * Time.deltaTime, 0, 0));
+        foreach (Transform child in transform)
+        {
+            if (child.transform.position.x >= pane_length*2)
+                child.transform.position = child.transform.position - new Vector3(pane_length * 3,0);
+            if (child.transform.position.x <= -pane_length * 2)
+                child.transform.position = child.transform.position + new Vector3(pane_length * 3, 0);
+        }
+    }
+}
